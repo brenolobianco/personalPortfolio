30670287f86d6a8fe0f71f927cf0b6d2ee75430d
Breno Lobianco <99978122+brenolobianco@users.noreply.github.com>
2022-11-01 16:39:54 -0300

    Add files via upload

--- src/components/footer/index.jsx
+++ src/components/footer/index.jsx
@@ -10,6 +10,7 @@ function Footer() {
     <li><a href="https://www.linkedin.com/in/breno-lobianco/">Linkedin</a></li>
     <li><a href="https://wa.me/5516996225832">Fale comigo no Whatsapp</a></li>
     <li><a href="https://github.com/brenolobianco">GitHub</a></li>
+    
 </ul>
 <button onClick={() => { window.scrollTo(0,0)}} >^</button>
     </FooterBox>
--- src/components/portfolio/index.jsx
+++ src/components/portfolio/index.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Hamburguer from "../../assets/hamburguer-kenzie.png";
 import KenzieHub from "../../assets/kenzie-hub.png";
 import NuKenzie from "../../assets/nu-kenzie.png";
+import Shop from "../../assets/shop.png";
 import { PortfolioSection, PortfolioDecription } from "./styles";
 
 function Portfolio() {
@@ -47,6 +48,27 @@ function Portfolio() {
         </div>
       </PortfolioDecription>
 
+      <PortfolioDecription>
+        <div className="left-side">
+          <img src={Shop} alt="img projeto"  />
+        </div>
+
+        <div className="right-side">
+          <h2>Mercado </h2>
+          <p>
+         Mercado De compras
+          </p>
+          <div className="btns">
+            <a href="https://curly-doodle-77c96492.pages.github.io/">
+              Visite
+            </a>
+            <a href="
+https://github.com/Kenzie-Academy-Brasil-Developers/entrega-lista-produtos-m2-sprint-1b-brenolobianco">
+              Repositorio
+            </a>
+          </div>
+        </div>
+      </PortfolioDecription>
       <PortfolioDecription>
         <div className="left-side">
           <img src={KenzieHub} alt="img projeto" srcset="" />
