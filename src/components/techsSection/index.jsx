f0cd35c276fa62ffc171a29e4b8ec5751a2564bc
BRENO LOBIANCO DOS SANTOS <breno.lobianco@gmail.com>
2023-05-03 11:26:59 -0300

    add

--- src/components/techsSection/index.jsx
+++ src/components/techsSection/index.jsx
@@ -6,7 +6,7 @@ import LogoLaravel from "../../assets/laravel.png";
 import LogoJs from "../../assets/js.png";
 import LogoTs from "../../assets/ts.png";
 import LogoPostGree from "../../assets/postgree.png";
-import LogoNode from "../../assets/node.png"
+import LogoNode from "../../assets/node.png";                                                                                                              import LogoVue from "../../assets/vue.jpg"
 
 
 function Techs() {
@@ -20,7 +20,7 @@ function Techs() {
         <img src={LogoJs} alt="js" />
         <img src={LogoTs} alt="ts" />
         <img src={LogoNode} alt="node" />
-        <img src={LogoPostGree} alt="postgree" />  
+        <img src={LogoPostGree} alt="postgree" />  <img src={LogoVue} alt="vue" />   
       </div>
     </TechsSection>
   );
